d03cb637207d7b4e2c060692d468bac3e6d39c81
976992537 <976992537@qq.com>
2016-11-03 14:55:52 +0800

    。

---
+++ app/src/main/java/com/example/wuxiang/mymi/activity/ChaojifanActivity.java
@@ -0,0 +1,15 @@
+package com.example.wuxiang.mymi.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.wuxiang.mymi.R;
+
+public class ChaojifanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chaojifan);
+    }
+}
